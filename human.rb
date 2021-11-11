2a628539ccb42ecaa96123829d24fde435b955bf
wataru.kakeno <wataru.kakeno@techacademy.jp>
2021-11-11 13:43:28 +0000

    人間

---
+++ human.rb
@@ -0,0 +1,12 @@
+require './thinkable'
+require './animal'
+class Human < Animal
+    include Thinkable
+    attr_accessor :hobby
+    
+    def initialize(name,age,hobby)
+        @name=name
+        @age=age
+       @hobby =hobby
+    end
+end    
---
+++ thinkable.rb
@@ -0,0 +1,7 @@
+module Thinkable
+    
+    def think
+        puts "私は#{@hobby}について考えています。"
+    end
+
+end
